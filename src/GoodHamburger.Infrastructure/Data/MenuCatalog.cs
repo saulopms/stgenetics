bd5ae5be41ca4442b123377b267f05fa093373b0
saulopms <saulopms@gmail.com>
2026-04-28 11:33:05 -0300

    pt-br

--- src/GoodHamburger.Infrastructure/Data/MenuCatalog.cs
+++ src/GoodHamburger.Infrastructure/Data/MenuCatalog.cs
@@ -8,11 +8,11 @@ public sealed class MenuCatalog : IMenuCatalog
 {
     private static readonly IReadOnlyList<MenuItem> _items =
     [
-        new MenuItem { Id = 1, Name = "X Burger", Price = 5.00m, Type = MenuItemType.Sandwich },
-        new MenuItem { Id = 2, Name = "X Egg",    Price = 4.50m, Type = MenuItemType.Sandwich },
+        new MenuItem { Id = 1, Name = "X Burguer", Price = 5.00m, Type = MenuItemType.Sandwich },
+        new MenuItem { Id = 2, Name = "X Ovo",    Price = 4.50m, Type = MenuItemType.Sandwich },
         new MenuItem { Id = 3, Name = "X Bacon",  Price = 7.00m, Type = MenuItemType.Sandwich },
-        new MenuItem { Id = 4, Name = "French Fries",  Price = 2.00m, Type = MenuItemType.Fries   },
-        new MenuItem { Id = 5, Name = "Soda",     Price = 2.50m, Type = MenuItemType.Soda    }
+        new MenuItem { Id = 4, Name = "Batata Frita", Price = 2.00m, Type = MenuItemType.Fries   },
+        new MenuItem { Id = 5, Name = "Refrigerante", Price = 2.50m, Type = MenuItemType.Soda    }
     ];
 
     private static readonly IReadOnlyDictionary<int, MenuItem> _index =
